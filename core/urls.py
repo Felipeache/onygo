6f535dfe8385fdd04b2c7bed22346cca2f1c1547
Felipe Henao <34544909+Felipeache@users.noreply.github.com>
2020-05-17 15:01:46 +0200

    sender changed

--- core/urls.py
+++ core/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', home, name='index'),
     path('chercher/', search, name='search'),
     path('mes-evenements/', my_events, name='my_events'),
-    path('mes-messages/<int:sender>/', show_messages, name='show_messages'),
+    path('mes-messages/<int:sender_id>/', show_messages, name='show_messages'),
     path('inbox/', show_senders, name='show_senders'),
     path('creer-evenement/', create_event, name='create_event'),
     path('envoyer-message/?sender=<sender>&receiver=<receiver>/', send_message, name='send_message'),
