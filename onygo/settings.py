c601e28b35388c2ae9fbb8f5d6453fa6e62369af
Felipe Henao <34544909+Felipeache@users.noreply.github.com>
2020-05-20 18:41:43 +0200

    permissions.HasAPIKey off

--- onygo/settings.py
+++ onygo/settings.py
@@ -64,7 +64,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
                 'rest_framework.permissions.IsAuthenticated',
-                "rest_framework_api_key.permissions.HasAPIKey",
+                #"rest_framework_api_key.permissions.HasAPIKey",
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
