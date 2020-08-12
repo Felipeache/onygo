dc839de64a29c0d792208bf4b1b563a18f5d5335
Felipe Henao <34544909+Felipeache@users.noreply.github.com>
2020-08-12 13:09:02 +0200

    removing delete_event_viewset

--- core/api/urls.py
+++ core/api/urls.py
@@ -3,7 +3,7 @@ from core.api.views import (
         get_event_viewset,
         event_list_viewset,
         update_event_viewset,
-        delete_event_viewset,
+        #delete_event_viewset,
         create_event_viewset,
         create_user_viewset,
         EnventsListApi
@@ -17,7 +17,7 @@ urlpatterns = [
     path('', EnventsListApi.as_view(), name='events_list_api'),
     path('<int:id>', get_event_viewset, name='get_event_viewset'),
     path('<int:id>/update/', update_event_viewset, name='update_event_viewset'),
-    path('<int:id>/delete/', delete_event_viewset, name='delete_event_viewset'),
+    #path('<int:id>/delete/', delete_event_viewset, name='delete_event_viewset'),
     path('create/', create_event_viewset, name='create_event_viewset'),
     # Registration:
 ]
