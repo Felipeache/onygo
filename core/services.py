bd92df84a594397b87308bcee100fbbcc6e9564d
Luis Felipe Henao <felipehenao86@gmail.com>
2021-04-20 21:14:16 +0200

    modify models + lat_lon created

---
+++ core/services.py
@@ -0,0 +1,46 @@
+from requests import post
+
+API_RESPONSE = ""
+
+
+def validate_address(event_address, city):
+    if len(event_address) == 0 or len(city) == 0:
+        return False
+    else:
+        event_address = event_address.replace(" ", "+").strip()
+        city = city.replace(" ", "+").strip()
+        geo_api = f"https://nominatim.openstreetmap.org/search?q={event_address}+{city}&format=json&polygon=1&addressdetails=1"
+        api_response = post(geo_api).json()
+        return True if len(api_response) > 0 else False
+
+
+def get_lat_lon(event_address, city):
+
+    event_address = event_address.replace(" ", "+").strip()
+    city = city.replace(" ", "+").strip()
+    geo_api = f"https://nominatim.openstreetmap.org/search?q={event_address}+{city}&format=json&polygon=1&addressdetails=1"
+    api_response = post(geo_api).json()
+    event_address, city = "", ""
+    return f"{api_response[0]['lat']} {api_response[0]['lon']}"
+
+
+def get_postal_code(event_address, city):
+    event_address = event_address.replace(" ", "+").strip()
+    city = city.replace(" ", "+").strip()
+    geo_api = f"https://nominatim.openstreetmap.org/search?q={event_address}+{city}&format=json&polygon=1&addressdetails=1"
+    api_response = post(geo_api).json()
+    event_address, city = "", ""
+    return f"{api_response[0]['address']['postcode']}"
+
+
+def decode_lat_lon(lat_lon):
+    lat, lon = lat_lon.split(" ")[0], lat_lon.split(" ")[1]
+    decode_api = (
+        f"https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json"
+    )
+    api_response = post(decode_api).json()
+    lat_lon = "", ""
+    return api_response["address"]
+
+
+print(validate_address("dfsdfs", ""))
--- core/views.py
+++ core/views.py
@@ -8,55 +8,57 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from random import randrange
 from .models import Message, Event, User, UserProfile, EventJoin
+from .services import get_lat_lon, get_postal_code
 from django.db.models import Q
 from itertools import chain
 from operator import attrgetter
 from .forms import (
-        Create_Event_Form,
-        CustomUserCreationForm,
-        UserProfile_Form,
-        EditProfileForm,
-        Send_Message_Form
-    )
+    Create_Event_Form,
+    CustomUserCreationForm,
+    UserProfile_Form,
+    EditProfileForm,
+    Send_Message_Form,
+)
 
 
 def view_404(request, *e):
-    return render(request, 'core/404.html')
+    return render(request, "core/404.html")
+
 
 # Search bar in the site:
 @login_required
 def search(request):
-    q = request.GET.get('q')
+    q = request.GET.get("q")
     if q:
-        events = Event.objects.filter(
-                    Q(city__icontains=q) |
-                    Q(event_name__icontains=q) |
-                    Q(event_description__icontains=q)
-        ).distinct().order_by('date')
-        paginator = Paginator(events, 5)
-        page = request.GET.get('page')
-        events = paginator.get_page(page)
-        return render(
-                request,
-                "core/chercher-evenement.html",
-                {"events": events}
+        events = (
+            Event.objects.filter(
+                Q(city__icontains=q)
+                | Q(event_name__icontains=q)
+                | Q(event_description__icontains=q)
+            )
+            .distinct()
+            .order_by("date")
         )
+        paginator = Paginator(events, 5)
+        page = request.GET.get("page")
+        events = paginator.get_page(page)
+        return render(request, "core/chercher-evenement.html", {"events": events})
     else:
         # messages.success(request, f'Resultats de ta recherche: {q}')
-        return (search_event(request))
+        return search_event(request)
 
 
 # Sending a new home img
 def home(request):
     sports = [
-            'jouer au Foot',
-            'te faire un Golf',
-            'jouer au Tennis',
-            'te faire un Jogging',
-        ]
+        "jouer au Foot",
+        "te faire un Golf",
+        "jouer au Tennis",
+        "te faire un Jogging",
+    ]
     xrandom = randrange(0, len(sports), 1)
     randomsport = str(sports[xrandom])
-    return render(request, 'core/index.html', {'randomsport': randomsport})
+    return render(request, "core/index.html", {"randomsport": randomsport})
 
 
 @login_required
@@ -65,11 +67,11 @@ def profile(request):
     try:
         user = User.objects.get(pk=id)
         userprofile = UserProfile.objects.get(user_id=id)
-        context = {'utilisateur': user, 'userprofile': userprofile}
-        return render(request, 'core/profile.html', context)
+        context = {"utilisateur": user, "userprofile": userprofile}
+        return render(request, "core/profile.html", context)
     except User.DoesNotExist:
         messages.error("Cet utilisateur n'existe plus")
-        return redirect('profile')
+        return redirect("profile")
 
 
 @login_required
@@ -77,57 +79,52 @@ def visit_profile(request, id):
     try:
         user = User.objects.get(pk=id)
         userprofile = UserProfile.objects.get(user_id=id)
-        context = {'utilisateur': user, 'userprofile': userprofile}
-        return render(request, 'core/profile.html', context)
+        context = {"utilisateur": user, "userprofile": userprofile}
+        return render(request, "core/profile.html", context)
     except User.DoesNotExist:
         messages.error(request, "Cet utilisateur n'existe pas")
-        return redirect('profile')
+        return redirect("profile")
     except "Entry.MultipleObjectsReturned":
         messages.error(request, "Plusieurs utilisateurs trouvés")
-        return redirect('profile')
+        return redirect("profile")
 
 
 @login_required
 def edit_profile(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
             form = EditProfileForm(request.POST, instance=request.user)
             profileForm = UserProfile_Form(
-                        request.POST,
-                        request.FILES,
-                        instance=request.user.userprofile
+                request.POST, request.FILES, instance=request.user.userprofile
             )
             if form.is_valid() and profileForm.is_valid():
                 form.save(commit=False)
                 profileForm.save(commit=True)
                 form.save(commit=True)
-                messages.success(request, 'Ton profil a été modifié :)')
-                return redirect('profile')
+                messages.success(request, "Ton profil a été modifié :)")
+                return redirect("profile")
         except Exception as e:
             error = str(e)
-            messages.error(
-                        request,
-                        f"Oops!!! nos lutins ont fait une betise. {error}"
-                    )
+            messages.error(request, f"Oops!!! nos lutins ont fait une betise. {error}")
     else:
         form = EditProfileForm(instance=request.user)
         profileForm = UserProfile_Form(instance=request.user.userprofile)
-        context = {'form': form, 'profileForm': profileForm}
-        return render(request, 'core/edit-profile.html', context)
+        context = {"form": form, "profileForm": profileForm}
+        return render(request, "core/edit-profile.html", context)
 
 
 @login_required
 def change_password(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PasswordChangeForm(data=request.POST, user=request.user)
         if form.is_valid():
             form.save()
             messages.success(request, "Ton mot de passe a été changé")
-            return redirect('index')
+            return redirect("index")
     else:
         form = PasswordChangeForm(user=request.user)
-        context = {'form': form}
-        return render(request, 'core/change-password.html', context)
+        context = {"form": form}
+        return render(request, "core/change-password.html", context)
 
 
 @login_required
@@ -135,9 +132,9 @@ def my_events(request):
     id = request.user.id
     event = get_object_or_404(Event, id=id)
     if event:
-        events = Event.objects.filter(owner=request.user.id).order_by('date')
+        events = Event.objects.filter(owner=request.user.id).order_by("date")
         paginator = Paginator(events, 5)
-        page = request.GET.get('page')
+        page = request.GET.get("page")
         events = paginator.get_page(page)
         return render(request, "core/liste-evenements.html", {"events": events})
 
@@ -147,17 +144,17 @@ def validate(request, id):
     try:
         event = Event.objects.get(id=id)
     except ObjectDoesNotExist:
-        messages.error(request, f'Cet événement n\'existe pas!')
-        return redirect('index')
+        messages.error(request, f"Cet événement n'existe pas!")
+        return redirect("index")
     if event.owner_id == request.user.id:
         events = EventJoin.objects.filter(event=id)
         accepted = EventJoin.objects.filter(event=id, accepted=True)
-        context = {'events': events, 'accepted': accepted}
+        context = {"events": events, "accepted": accepted}
 
         return render(request, "core/valider-demandes.html", context)
     else:
-        messages.error(request, f'Cet événement ne t\'appartiens pas!')
-        return redirect('index')
+        messages.error(request, f"Cet événement ne t'appartiens pas!")
+        return redirect("index")
 
 
 @login_required
@@ -167,11 +164,11 @@ def accept(request, guest_id, event_id):
     if not ev.accepted:
         ev.accepted = True
         ev.save()
-        messages.success(request, f'{guest} a été accepté')
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        messages.success(request, f"{guest} a été accepté")
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
     else:
-        messages.error(request, f'{guest} a déjà été accepté')
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        messages.error(request, f"{guest} a déjà été accepté")
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
 # Showing the events in the same city than the user
@@ -179,47 +176,43 @@ def accept(request, guest_id, event_id):
 def search_event(request):
     user_id = request.user.id
     u = UserProfile.objects.get(user_id=user_id)
-    events = Event.objects.filter(city=u.city).order_by('date')
-    context = {'events': events}
-    return render(request, 'core/liste-evenements.html', context)
+    events = Event.objects.filter(city=u.city).order_by("date")
+    context = {"events": events}
+    return render(request, "core/liste-evenements.html", context)
 
 
 @login_required
 def send_message(request, sender, receiver):
     receiver = UserProfile.objects.get(id=receiver)
     sender = UserProfile.objects.get(user_id=request.user.id)
-    data = {'form': Send_Message_Form(), 'receiver': receiver}
-    if request.method == 'POST':
+    data = {"form": Send_Message_Form(), "receiver": receiver}
+    if request.method == "POST":
         form = Send_Message_Form(request.POST)
         if form.is_valid():
-            msg = Message(
-                        sender=sender,
-                        receiver=receiver,
-                        text = request.POST.get('text')
-                        )
+            msg = Message(sender=sender, receiver=receiver, text=request.POST.get("text"))
             msg.save()
-            messages.success(request, 'Ton message a été envoyé!')
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+            messages.success(request, "Ton message a été envoyé!")
+            return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
         else:
-            data['form'] = form
+            data["form"] = form
             messages.error(
-                            request,
-                            f'Oops {request.user}! ton message n\'a pas été envoyé :C',
-                            form.errors
-                        )
-    return render(request, 'core/envoyer-message.html', data)
+                request,
+                f"Oops {request.user}! ton message n'a pas été envoyé :C",
+                form.errors,
+            )
+    return render(request, "core/envoyer-message.html", data)
 
 
 @login_required
 def show_senders(request):
     me = UserProfile.objects.get(user_id=request.user.id)
     context = []
-    msgs = Message.objects.filter(receiver_id=me).values('sender').distinct()
+    msgs = Message.objects.filter(receiver_id=me).values("sender").distinct()
     for sender in msgs:
         value = sender.values()
         for id in value:
             context += UserProfile.objects.filter(id=id)
-    return render(request, "core/inbox.html", {'context': context})
+    return render(request, "core/inbox.html", {"context": context})
 
 
 @login_required
@@ -228,27 +221,23 @@ def show_messages(request, sender_id):
     sender = UserProfile.objects.get(user_id=sender_id)
     received_msgs = []
     sent_msgs = []
-    received_msgs = Message.objects.filter(
-                    sender=sender,
-                    receiver=me).distinct().values(
-                    'sent',
-                    'text',
-                    'sender').order_by('sent')
-    sent_msgs = Message.objects.filter(
-                    sender=me,
-                    receiver=sender).distinct().values(
-                    'sent',
-                    'text',
-                    'sender').order_by('sent')
+    received_msgs = (
+        Message.objects.filter(sender=sender, receiver=me)
+        .distinct()
+        .values("sent", "text", "sender")
+        .order_by("sent")
+    )
+    sent_msgs = (
+        Message.objects.filter(sender=me, receiver=sender)
+        .distinct()
+        .values("sent", "text", "sender")
+        .order_by("sent")
+    )
     msgs_list = list(chain(received_msgs, sent_msgs))
     return render(
-                request,
-                "core/show-message.html",
-                {
-                    "msgs_list": msgs_list,
-                    "sent_msgs": sent_msgs,
-                    "sender": sender
-                }
+        request,
+        "core/show-message.html",
+        {"msgs_list": msgs_list, "sent_msgs": sent_msgs, "sender": sender},
     )
 
 
@@ -257,57 +246,53 @@ def apply(request, id):
 
     user_name = request.user.first_name
     user_id = request.user.id
-    if request.method == 'GET':
+    if request.method == "GET":
         try:
             guest = UserProfile.objects.get(user_id=user_id)
             ev = EventJoin(event=Event.objects.get(id=id), guest=guest)
             ev.save()
-            messages.success(
-                        request,
-                        f"{user_name}, ta demande a été envoyée!"
-            )
-            return redirect('search_event')
+            messages.success(request, f"{user_name}, ta demande a été envoyée!")
+            return redirect("search_event")
         except Exception as e:
             error = str(e)
             if error:
-                messages.error(
-                                request,
-                                "Oops!!! tu as déjà postulé à cet événement"
-                            )
-                return redirect('search_event')
+                messages.error(request, "Oops!!! tu as déjà postulé à cet événement")
+                return redirect("search_event")
             else:
                 messages.error(
-                                request,
-                                f"Oops!!! nos lutins ont fait une betise. \
-                                {error}"
-                            )
-                return redirect('search_event')
+                    request,
+                    f"Oops!!! nos lutins ont fait une betise. \
+                                {error}",
+                )
+                return redirect("search_event")
 
 
 @login_required
 def create_event(request):
-    data = {'form': Create_Event_Form()}
-    if request.method == 'POST':
+    data = {"form": Create_Event_Form()}
+    if request.method == "POST":
         form = Create_Event_Form(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.owner = request.user
-            post.save()
+            event = form.save(commit=False)
+            event.owner = request.user
+            event.event_lat_lon = get_lat_lon(event.event_address, event.city)
+            event.zip_code = get_postal_code(event.event_address, event.city)
+            event.save()
             messages.success(
-                                request,
-                                f'Merci {request.user}!\
-                                ton événement a été crée <3'
-                        )
-            return redirect('index')
+                request,
+                f"Merci {request.user}!\
+                                ton événement a été crée <3",
+            )
+            return redirect("index")
         else:
-            data['form'] = form
+            data["form"] = form
             messages.error(request, form.errors)
             messages.error(
-                            request,
-                            f'Oops {request.user}! ton événement n\'a\
-                            pas été crée :C'
-                        )
-    return render(request, 'core/creer-evenement.html', data)
+                request,
+                f"Oops {request.user}! ton événement n'a\
+                            pas été crée :C",
+            )
+    return render(request, "core/creer-evenement.html", data)
 
 
 def create_user(request):
@@ -324,11 +309,11 @@ def create_user(request):
         else:
             form = CustomUserCreationForm()
             ProfileForm = UserProfile_Form()
-            context = {'form': form, 'profileForm': ProfileForm}
+            context = {"form": form, "profileForm": ProfileForm}
             messages.error(request, "Il y a une erreur dans le formulaire")
             return render(request, "registration/register.html", context)
     else:
         form = CustomUserCreationForm()
         ProfileForm = UserProfile_Form()
-    context = {'form': form, 'profileForm': ProfileForm}
-    return render(request, 'registration/register.html', context)
+    context = {"form": form, "profileForm": ProfileForm}
+    return render(request, "registration/register.html", context)
