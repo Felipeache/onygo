074b3b9ae5c1e60a11468dc6bd10c19e17c195c5
Felipe Henao <34544909+Felipeache@users.noreply.github.com>
2021-10-29 00:55:50 +0200

    Update settings.py

--- onygo/settings.py
+++ onygo/settings.py
@@ -14,13 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+SECRET_KEY = os.getenv('SECRET_DJANGO')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
