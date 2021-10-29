ba45b93e4f18a63d9143501bcf77b227e85dc92f
Luis Felipe Henao <felipehenao86@gmail.com>
2021-10-29 12:31:02 +0200

    debuf

--- onygo/settings.py
+++ onygo/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 
 key_path = "onygo/secret.txt" if DEBUG else "/home/holiholi/onygo/onygo/secret.txt"
 with open(key_path) as f:
